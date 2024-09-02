c9f9f53dc7116a439f5858dc123e7d9091d89b74
nkls4223 <nkls@weltrevolution.info>
2024-09-02 16:09:28 +0200

    Nano-Stromampel an Nano Every angepasst

--- Ampelsteckdose_Nano/Ampelsteckdose_Nano.ino
+++ Ampelsteckdose_Nano/Ampelsteckdose_Nano.ino
@@ -2,8 +2,8 @@
 #include <UIPEthernet.h>
 #include <ArduinoHttpClient.h>
 
-IPAddress ip(192,168,1,207);
-IPAddress myDns(192,168,0,1);
+//IPAddress ip(192,168,1,207);
+//IPAddress myDns(192,168,0,1);
 EthernetClient client;
 HttpClient http_client = HttpClient(client, "alfons.siebenlinden.net", 80);
 char server[] = "alfons.siebenlinden.net";
@@ -14,10 +14,21 @@ char server[] = "alfons.siebenlinden.net";
 #define RELAIS 6
 //Digital Pin 4 ist intern belegt! Darum Pin 5 für grün!
 
-unsigned long relaydelay=900000; //delay for the relay
+
+/*unsigned long relaydelay=000; //delay for the relay
 unsigned long lasttimer;
 unsigned long timepassed;  
 int relais_is_on=0;
+*/
+
+
+unsigned long ms_since_relais_changed=0;
+unsigned long ms_since_time_update=0;
+int relais_state=0;
+unsigned long ms_power_cycle=300000;
+int power_cycle_granted=1;
+int led_state=0;
+int first_run=1;
 
 void setup() 
 {
@@ -48,7 +59,7 @@ void setup()
          {
          Serial.println("Ethernet cable is not connected.");
          }
-      Ethernet.begin(mac, ip, myDns);
+      //Ethernet.begin(mac, ip, myDns);
       } 
    else 
       {
@@ -57,32 +68,112 @@ void setup()
       }
    //delay(1000);
   
-lasttimer=millis();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+  {
   Serial.print("GET 7lenergy ...");
   http_client.get("/7lenergy/energy.json");
-
-  // read the status code and body of the response
-  //int statusCode = http_client.responseStatusCode();
   String response = http_client.responseBody();
-
   int start_at = response.lastIndexOf(":");
   String value = response.substring(start_at + 3, response.length() - 3);
-  //String value=response;
-  //Serial.println("extracted value " + value);
-  
-  //Serial.println("parsed value: ");
-  //Serial.println(value.toInt());
   int energy_balance = value.toInt();
-  Serial.print("VALUE=");
+  /*Serial.print("VALUE=");
   Serial.print(energy_balance);
-  Serial.print(",");
+  Serial.print(",");*/
 
-if((energy_balance > 0) && (energy_balance < 1000 )) {
+  Serial.println("RESPONSE");
+  Serial.println(response);
+  Serial.println("VALUE");
+  Serial.println(value);
+  Serial.println("ENERGYBALANCE");
+  Serial.println(energy_balance);
+
+
+  ms_since_time_update=ms_since_time_update + (millis()-ms_since_time_update);
+  ms_since_relais_changed=ms_since_relais_changed + ms_since_time_update;
+  ms_since_time_update=0;
+
+
+  if (ms_since_relais_changed >= ms_power_cycle)
+    {
+    power_cycle_granted=1;
+    }
+  else
+    {
+    power_cycle_granted=0;  
+    }
+
+  if (first_run==1)
+    {
+    power_cycle_granted=1;  
+    }
+  Serial.println("");
+  Serial.print("preset for power cycle delay: ");
+  Serial.println(ms_power_cycle);
+  Serial.print("First Run: ");
+  Serial.println(first_run);
+  Serial.print("Time since relais changed: ");
+  Serial.println(ms_since_relais_changed);
+  Serial.print("Power Cycle Granted: ");
+  Serial.println(power_cycle_granted);
+
+
+ 
+
+  if(energy_balance <= 0) 
+    {
+    if ((power_cycle_granted==1) && (relais_state==1))
+      {
+      digitalWrite(RELAIS, LOW);
+      relais_state=0;
+      ms_since_relais_changed=0;
+      }
+    Serial.println("RED");
+    led_state=0;
+    digitalWrite(RED_LED, HIGH); 
+    digitalWrite(YELLOW_LED, LOW);
+    digitalWrite(GREEN_LED, LOW); 
+    }
+  
+  
+  if((energy_balance > 0) && (energy_balance < 1000 )) 
+    {
+    if ((power_cycle_granted==1) && (relais_state==1))
+      {
+      digitalWrite(RELAIS, LOW);
+      relais_state=0;
+      ms_since_relais_changed=0;
+      }
+    Serial.println("YELLOW");
+    led_state=1;
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(YELLOW_LED, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+    }
+
+  if(energy_balance >= 1000) 
+    {
+    if (relais_state==0)
+      {
+      digitalWrite(RELAIS, HIGH);
+      relais_state=1;
+      ms_since_relais_changed=0;
+      first_run=0;
+      }
+    
+    Serial.println("GREEN");
+    led_state=2;
+    digitalWrite(RED_LED, LOW); 
+    digitalWrite(YELLOW_LED, LOW);
+    digitalWrite(GREEN_LED, HIGH);
+    }
+
+    Serial.print("Relais State: ");
+    Serial.println(relais_state);
+
+/*if((energy_balance > 0) && (energy_balance < 1000 )) {
     Serial.println("YELLOW");
     digitalWrite(RED_LED, LOW);
     digitalWrite(YELLOW_LED, HIGH);
@@ -122,7 +213,7 @@ if(energy_balance <= 0) {
       }
     lasttimer=millis();
   }
-      
+ */     
   
   delay(10000);
 }
