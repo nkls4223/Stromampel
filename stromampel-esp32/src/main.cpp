8ca70bad5620d58e830633737c49c6f84c2c78f7
nkls4223 <nkls4223@github.com>
2025-01-01 15:28:19 +0100

    activating LEDs

--- stromampel-esp32/src/main.cpp
+++ stromampel-esp32/src/main.cpp
@@ -2,19 +2,33 @@
 #include <WiFi.h>
 #include <Ethernet.h>
 
+
+int LED_NETWORK = 33;
+int LED_BLUE = 32;
+int LED_RED = 2;
+int LED_YELLOW = 14;
+int LED_GREEN = 15;
+
 String WIFI_SSID = "0";
 String WIFI_PWD = "fn0rd4223";
 
 uint8_t MAC_eth[6];
 uint8_t MAC_wifi[6];
 
-EthernetClient LANclient;
+
+enum Signal_Status
+   {
+   RED = 1,
+   YELLOW = 2,
+   GREEM = 3,
+   };
 
 bool InitWiFi()
    {
    if(WiFi.status() == WL_CONNECTED)
       {
       return(1);
+      
       }
    else
       {
@@ -40,47 +54,89 @@ bool InitLAN()
       }
    else
       {
-      if(Ethernet.begin(MAC_eth))
-         {
-         return(1);
-         }
-      else
-         {
-         return(0);
-         }
+      Ethernet.begin(MAC_eth);
+      delay(1000);
+         if(Ethernet.localIP()!=0)
+            {
+            return(1);
+            }
+         else
+            {
+            return(0);
+            }
       }
    }
 
-
+void testLEDs()
+   {
+   int i=0;
+   Serial.println("Testing LEDs...");
+   while(i<15)
+      {
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_BLUE, LOW);
+      delay(30);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_YELLOW, HIGH);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_BLUE, LOW);
+      delay(30);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(LED_BLUE, LOW);
+      delay(30);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_BLUE, HIGH);
+      delay(30);
+      digitalWrite(LED_BLUE, LOW);
+      i++;
+      }
+   }
+/*=======================================================================*/
 
 void setup()
    {
+
    Serial.begin(9600);
    delay(5000);
 
+   pinMode(LED_NETWORK, OUTPUT);
+   pinMode(LED_RED, OUTPUT);
+   pinMode(LED_YELLOW, OUTPUT);
+   pinMode(LED_GREEN, OUTPUT);
+   pinMode(LED_BLUE, OUTPUT);
    //get MAC Addresses
    esp_read_mac(MAC_eth, ESP_MAC_ETH);
    Serial.print("ETH MAC: "); for (int i = 0; i < 5; i++) {Serial.printf("%02X:", MAC_eth[i]);} Serial.printf("%02X\n", MAC_eth[5]);
    esp_read_mac(MAC_wifi, ESP_MAC_WIFI_STA);
    Serial.print("WIFI MAC: ");   for (int i = 0; i < 5; i++) {Serial.printf("%02X:", MAC_wifi[i]);} Serial.printf("%02X\n", MAC_wifi[5]);
-
+//   Ethernet.init(33);
    }
 
 
 void loop()
    {
 
+   digitalWrite(LED_NETWORK, LOW); 
+   testLEDs();
+   delay(1000);
    if(InitWiFi())
       {
       Serial.print("...WiFi is ON, IP is:");
       Serial.println(WiFi.localIP());
+      digitalWrite(LED_NETWORK, HIGH);
       }
    else
       {
       Serial.println("...Wifi is OFF");
       }
 
-   if (InitLAN)
+   if (InitLAN())
       {
       Serial.print("...LAN is ON, IP is: ");
       Serial.println(Ethernet.localIP());
@@ -92,42 +148,7 @@ void loop()
 
 
 
-/*   if(WiFi.status() == WL_CONNECTED)
-        {
-        Serial.println("Connected");
-        }
-     else
-        {
-        Serial.println("Not connected");
-        delay(2000);
-        }
-*/
-   Serial.println("Loop has looped");
-
-/*   if (Ethernet.begin(mac) == 0) 
-      {
-      Serial.println("Failed to configure Ethernet using DHCP");
-      if (Ethernet.hardwareStatus() == EthernetNoHardware) 
-         {
-         Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-         while (true) 
-            {
-            delay(1);
-            }
-         }
-      if (Ethernet.linkStatus() == LinkOFF) 
-         {
-         Serial.println("Ethernet cable is not connected.");
-         }
-      //Ethernet.begin(mac, ip, myDns);
-      } 
-   else 
-      {
-      Serial.print("  DHCP assigned IP ");
-      Serial.println(Ethernet.localIP());
-      }
-*/  
-   delay(1000);
+   delay(5000);
    }
 
 
